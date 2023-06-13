7576ded198aff05011a2ff0ac6bd6f582f814ba1
Pete Carapetyan <pete.carapetyan@gmail.com>
2023-06-13 18:35:31 -0500

    Remove all directories except some

--- hibernate-example/src/main/java/com/coderscampus/week18/hibernateexample/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.coderscampus.week18.hibernateexample.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.coderscampus.week18.hibernateexample.domain.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-	
-}
