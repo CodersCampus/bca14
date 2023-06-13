c2f1aca6ac4a0e42154aa9ef5c193a31f3bed040
Pete Carapetyan <pete.carapetyan@gmail.com>
2023-06-13 18:35:37 -0500

    Remove all directories except some

--- MySQL/lesson_4.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `transactions` (
-  `transaction_id` INT NOT NULL,
-  `transaction_date` DATETIME NULL,
-  `amount` DECIMAL(2) NULL,
-  `type` VARCHAR(1) NULL,
-  PRIMARY KEY (`transaction_id`));
