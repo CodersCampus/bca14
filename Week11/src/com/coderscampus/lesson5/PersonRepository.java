5e7ae954e95e28ee3b660385adcb012c41630cf3
Pete Carapetyan <pete.carapetyan@gmail.com>
2023-06-13 18:35:43 -0500

    Remove all directories except some

--- Week11/src/com/coderscampus/lesson5/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-package com.coderscampus.lesson5;
-
-public class PersonRepository {
-	public void save (Person person) {
-		System.out.println("saving the person obj");
-	}
-}
--- Week7/src/com/coderscampus/lesson1/FileApplication.java
+++
@@ -1,8 +0,0 @@
-package com.coderscampus.lesson1;
-
-public class FileApplication {
-	public static void main (String[] args) {
-		
-		FileService fileService = new FileServiceImpl();
-	}
-}
